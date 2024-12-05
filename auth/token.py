d68f8d7ac3a8b6cf4436b54d098093b8920eefe1
Tuấn <147260307+Yumeio@users.noreply.github.com>
2024-12-06 00:28:59 +0700

    🔨  update: auth classifiel sample

--- auth/token.py
+++ auth/token.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Literal
 
-class Token(BaseModel):
+TOKEN_TYPE = Literal["admin", "user"]
+    
+class Token():
     Id: int
     UserID: int
     NameToken: str
     Status: bool
     Key: str
-    TokenType: str
+    TokenType: TOKEN_TYPE
     CreatedAt: str
     AccessedTime: str
     ExpiredTime: str
@@ -16,8 +18,35 @@ class Token(BaseModel):
     QuotaResetTime: str
     Models: List[str]
     Subnet: str
-
     
-    
-    
-
+    def __init__(
+        self,
+        Id: int,
+        UserID: int,
+        NameToken: str,
+        Status: bool,
+        Key: str,
+        TokenType: TOKEN_TYPE,
+        CreatedAt: str,
+        AccessedTime: str,
+        ExpiredTime: str,
+        RemainQuota: int,
+        LimitedQuota: int,
+        QuotaResetTime: str,
+        Models: List[str],
+        Subnet: str
+    ) -> None:
+        self.Id = Id
+        self.UserID = UserID
+        self.NameToken = NameToken
+        self.Status = Status
+        self.Key = Key
+        self.TokenType = TokenType
+        self.CreatedAt = CreatedAt
+        self.AccessedTime = AccessedTime
+        self.ExpiredTime = ExpiredTime
+        self.RemainQuota = RemainQuota
+        self.LimitedQuota = LimitedQuota
+        self.QuotaResetTime = QuotaResetTime
+        self.Models = Models
+        self.Subnet = Subnet
--- auth/user.py
+++ auth/user.py
@@ -1,13 +1,35 @@
-from pydantic import BaseModel
-
-class User(BaseModel):
-    Id: int
-    BindID: str
+class User():
+    Id: int            
+    BindID: str         
     Username: str
     Password: str
     Email: str
+    DisplayName: str
     Token: str
-    Blacklist: bool
     CreatedAt: str
     UpdatedAt: str
+    Blacklist: bool
     
+    def __init__(
+        self,
+        Id: int,
+        BindID: str,
+        Username: str,
+        Password: str,
+        Email: str,
+        DisplayName: str,
+        Token: str,
+        CreatedAt: str,
+        UpdatedAt: str,
+        Blacklist: bool = False
+    ) -> None: 
+        self.Id = Id
+        self.BindID = BindID
+        self.Username = Username
+        self.Password = Password
+        self.Email = Email
+        self.DisplayName = DisplayName
+        self.Token = Token
+        self.CreatedAt = CreatedAt
+        self.UpdatedAt = UpdatedAt
+        self.Blacklist = Blacklist
